5d4ad4104029c8318860a5d2253c12184d139007
Tadeas Petak <tadeas.petak@jayway.com>
2016-11-30 18:28:04 +0100

    Change prod to production.

--- server/front/index.js
+++ server/front/index.js
@@ -58,7 +58,7 @@ function renderPage(props, store){
 
         <link rel="icon" type="image/png" href="/media/favicon.ico">
         <title>Isomorphic Demo</title>
-        ${process.env.NODE_ENV === 'prod' ? '<link rel="stylesheet" type="text/css" href="/screen.css">' : ''}
+        ${process.env.NODE_ENV === 'production' ? '<link rel="stylesheet" type="text/css" href="/screen.css">' : ''}
       </head>
       <body>
         <div id="app"><div>${app}</div></div>
