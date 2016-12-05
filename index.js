0512752de6281c4130d09ff69ef565b0ae908e49
Tadeas Petak <tadeas.petak@jayway.com>
2016-12-05 10:46:31 +0100

    Replace PostgreSQL with SQLite.

--- index.js
+++ index.js
@@ -10,12 +10,23 @@ const db = require('./server/db');
 //necessary in order for the stylesheets not to be loaded in the `./client/index.jsx`
 delete process.env.BROWSER;
 
+// ensure that English and Swedish exist
+let ensureLanguages = () => {
+  return db.Language.findAll().then(languages => {
+    if(languages.length) return;
+    return db.Language.create({id: 1, name: 'English'})
+      .then(() => db.Language.create({id: 2, name: 'Swedish'}));
+  });
+};
+
 db.sequelize.sync().then(() => {
-  let port = process.env.PORT || 3600;
-  let server = require('./server').listen(port,() => {
-    let address = server.address().address;
-    let port = server.address().port;
-    console.log(`Isomorphic Demo listening at http://${address}:${port}`);
+  ensureLanguages().then(() => {
+    let port = process.env.PORT || 3600;
+    let server = require('./server').listen(port, () => {
+      let address = server.address().address;
+      let port = server.address().port;
+      console.log(`Isomorphic Demo listening at http://${address}:${port}`);
+    });
   });
 }, error => {
   console.log(`Unable to sync the database: ${error}.`);
